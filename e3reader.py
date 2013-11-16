b14f544d8d2c40f508eb9a83798c151ed88a5411
Hans-Christoph Steiner <hans@eds.org>
2013-11-15 21:42:30 -0500

    convert E3Reader to be used as instances built by parsing the data
    Then you access the data as instance properties, i.e. e3.firstname.

--- e3reader.py
+++ e3reader.py
@@ -1,28 +1,87 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# this is based on the document "E3 Wedge Reader Programmer's Manual Rev A.7.pdf"
+import re
 
 class E3Reader():
     '''class for handling the Heartland E3 credit card reader'''
 
+    FIELD_DELIMITER = chr(0x7C)
+
+    def __init__(self, data):
+        '''initialize an instance with a full swipe'''
+        if not E3Reader.isvalid(card):
+            raise Exception('Not valid card data: ' + card)
+        self.data = data
+        self._parse()
+
     @staticmethod
-    def parsename(card):
-        '''given a complete string from a Heartland E3 reader, return the firstname and lastname'''
-        if card[0] != '<' or card[-1] != '>':
-            print 'Not valid card data: ' + card
-            return
-        name = card.split('|')[0].split('^')[1].lower()
-        names = name.split('/')
-        if len(names) == 2:
-            lastname = names[0].lstrip().rstrip().capitalize()
-            firstname = names[1].lstrip().rstrip().capitalize()
-        elif len(names) == 1:
-            firstname = ''
-            lastname = name.lstrip().rstrip().capitalize()
+    def isvalid(card):
+        '''validate that we got a complete packet'''
+        if re.match("^<X1.*\|>$", card):
+            # "X1 = Error condition exist"
+            return False
+        if re.match("^<E1.+\|>$", card):
+            # E1 = Regular data output
+            if len(card.split('|')) != 11:
+                return False
+            return True
+        return False
+
+    def _parse(self):
+        '''given a complete string from a Heartland E3 reader, parse data into a dict'''
+        self.fields = card.split('|')
+        self.track1 = None
+        self.track2 = None
+        self.number = None
+        self.expirationdate = None
+        if len(self.fields[0]) > 9 and self.fields[0][9] == '%':
+            self.track1 = '%' + self.fields[0].split('%')[1].split('?')[0] + '?'
+            self.track1formatcode = self.track1[1]
+            track1data = self.track1.split('^')
+            self.number = re.sub('[^0-9]', '', track1data[0][2:])
+            self.name = track1data[1]
+            self.expirationdate = track1data[2][0:4]
+            names = self.name.split('/')
+            if len(names) == 2:
+                self.lastname = names[0].lstrip().rstrip().capitalize()
+                self.firstname = names[1].lstrip().rstrip().capitalize()
+            elif len(names) == 1:
+                self.firstname = ''
+                self.lastname = self.name.lstrip().rstrip().capitalize()
+            else:
+                self.firstname = ''
+                self.lastname = ''
+        if len(self.fields[3]) > 2 and self.fields[3][2] == ';':
+            self.track2 = ';' + self.fields[3].split(';')[1].split('?')[0] + '?'
+            track2data = self.track2.split('=')
+            if self.number == None:
+                self.number = track2data[0].split(';')[1]
+            if self.expirationdate == None:
+                self.expirationdate = track2data[1][0:4]
+        if re.match('3[08]', self.number[0:2]):
+            self.type = 'dinersclub'
+        elif self.number[0:2] == '35':
+            self.type = 'jcb'
+        elif self.number[0:2] == '37':
+            self.type = 'americanexpress'
+        elif self.number[0] == '4':
+            self.type = 'visa'
+        elif self.number[0:2] == '50':
+            self.type = 'dankort'
+        elif re.match('5[1-5]', self.number[0:2]):
+            self.type = 'mastercard'
+        elif self.number[0:2] == '56':
+            self.type = 'australianbankcard'
+        elif self.number[0:2] == '60':
+            self.type = 'discover'
+        elif self.number[0:2] == '63':
+            self.type = 'switchsolo'
+        elif self.number[0] == '7':
+            self.type = 'dankort'
         else:
-            firstname = ''
-            lastname = ''
-        return firstname, lastname
+            self.type = 'UNKNOWN'
 
 
 if __name__ == '__main__':
@@ -30,10 +89,12 @@ if __name__ == '__main__':
     # data I faked so it has full names
     fakedata = '''<E1050811%B4888900000008888^STEINER/HANS-CHRISTO^131100000000000000000?|qwt0+QpJUgQvcVlI9MimCLW0KUcBV7ccf/qTg8/qh3NY4D1sx53if4AASd/r|+++++++EeBFsgfSl|11;4888900000008888=131100000000000?|6rc8dpYmsFHtpIgTyLLpmbWxF2M|+++++++EeBFosfSl|00|||/wECAQECAoFGsgEH3AELTDT6jRZwb3NAc2VjdXJlZXhjaGFuZ2UubmV0u5elwS+b/8HwW7RluF6gIRs447/4SXo0sSw27V1SFOC90dVWpOuB7CgsQ4ERzyhvCA8+/IUmvqsaJK2E+qSHOSGk4XFnysGeiZlSo2jCuc0gvamjjMXrvtlPByXonqwpjJVtFQTOFIxGXpSxOoqr/Sq/gJbb5jCanA4ZiDYVXjgAjhg0+iAJRwUOzCgT+Urq+PMKoYMK5BTyCh0e/jb6oboOJnOq|>'''
     morefakedata = '''<E1052411%B4588240000002202^STEINER/CHRIS ^1211000000000000000000000000000?|vIKypvzcmTtYme8w6bCg12BxsjuzYO5zs56r+tWy7maokt6j5gnzI/XtTv8Fdf4ViE6E|+++++++7SspSskLx|11;4588240000002202=121100000000000?|AY+qjai0Wv6Dm4NwFmwanUmm7Wu|+++++++7SspSjkLx|00|||/wECAQECAoFGAgEH3AELTsT6jRZwb3NAc2VjdXJlZXhjaGFuZ2UubmV0u5elwS+b/8HwW7RluF6gIRs447/4SXo0fSw27V1SFOC90dVWpOuB7CgcQ4ERzyhvCA8+/IUmvqsaJK2E+qSHOSGk4XFnysGeiZlSo2jCuc0gvamjjMXrvtlPByXonqspjJVtFQTOFIxGXpSxOoqr/Sq/gJbb5jCanA4ZiDYVXjgAjhg0+iAJRwUOzCgT+Urq+PMKoYMK5BTyCh0e/jb6oboOJnOq|>'''
+    notevenacreditcard = '''<E1033600|||20;813631526=3227?|7ehyjRUBc6p2Ckr3l44OUqhjkCx||00|||/wECAQECAoFGAgEH3QsPTDT6jRZwb3NAc2VjdXJlZXhjaGFuZ2UubmV0lAym4RaaasMpmwHUjeEhbcq+EIMVG8+xo+/YVzU2h3CF25CF7+L/DqELzZYl9m9Y4JJtZ2vsMeRkdk6u6W4t3AG1HHS2fU08eMQeaUv77+t0aT1zDaQAaGfIodBDOpDXksU0EP3an5H59Uikg/T8dzbOm/I+bRbYcalObc3g7I8AGoYvWFvAYtNfzPYcXbu5V+BDqoXUnfPl3Z/wX7yj53t8lAd1|>'''
 
     cards = []
     cards.append(fakedata)
     cards.append(morefakedata)
+    cards.append(notevenacreditcard)
 
     f = open('testdata.txt', 'r')
     lines = f.readlines()
@@ -41,5 +102,14 @@ if __name__ == '__main__':
         cards.append(line.rstrip('\n'))
 
     for card in cards:
-        firstname, lastname = E3Reader.parsename(card)
-        print '"first: "' + firstname + '" last: "' + lastname + '"'
+        if E3Reader.isvalid(card):
+            print('-----------------------------------')
+            e = E3Reader(card)
+            print(e.number)
+            if e.track1 != None:
+                print('track1'),
+                print(e.firstname + " " + e.lastname + ": " + '(' + e.type + ') '),
+                print(' exp: ' + e.expirationdate)
+            if e.track2 != None:
+                print('track2'),
+                print(e.track2)
