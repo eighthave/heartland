73bf3d72effa32428707deb89a308059cda444d1
Hans-Christoph Steiner <hans@eds.org>
2013-11-15 22:42:08 -0500

    flush out __init__.py

--- heartland/__init__.py
+++ heartland/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+'''module for working with credit cards via Heartland Payment Systems'''
 
 __version__   = 0.1
 __author__    = "Hans-Christoph Steiner"
+__all__ = ['e3reader', 'posgateway']
